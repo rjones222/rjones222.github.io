eb14bff45e42462923a8cd563427ddf06a12613b
Rick Jones <rick.jones@saatchionline.com>
2016-08-14 19:09:55 -0400

    fix typo, angular keypress

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ angular.module('website', ['ngAnimate', 'ngRoute'])
             'about': {
                 label: 'Rick W. Jones',
                 sublabel: 'About',
-                content: 'I’m a UI Developer and Front End Software Engineer with more than a decade of experience crafting rapid prototypes, high volume pixel-perfect advertising and marketing sites, and elegant, user-friendly responsive interfaces for online marketplaces. My skill set includes expert level CSS and HTML, JavaScript, jQuery, and PHP (Zend 1.12 and Laravel). Additional tools include Adobe CC and a strong sense of typography and design, MySQL, Git, Sass, Foundation 5, Bootstrap, Bower and Grunt. I’m in the process of learning Node.js and React, too. When I am not busy coding, some of the things I like to do best are playing guitar and my stand up bass, home recording, or watching my son play baseball. I also love going out to see live music and comedy.<br><br>For more details about my experience, please see <a href="/assets/rickwjones_res_2016_NC.pdf" target="_blank" title="PDF resume for Rick W. Jones">my resume</a> or visit <a href="http://www.linkedin.com/pub/rick-jones/2/893/563/en" target="_blank">my LinkedIn page</a>.<br><br><div class="credit"><a href="https://www.flickr.com/photos/diversey/5800134409/in/photolist-9QxcTp-77RgMd-4DzDuA-6cq64C-6ckToX-gG9NrM-45GWU1-8yxDLp-94HXqq-eL7G5C-9EQp1w-9irQkD-a8ek2A-4Ub7AR-a8ejF9-a88npx-f6C4sR-nsdCV3-5J3Sr4-7qDFP8-6bMhqd-8T69tw-fnjckt-93j1vo-eWRK6c-gG9Q2a-5vUnQD-93FkxB-79YrQ5-3UHHsL-616R88-eJepFP-857WUC-9a9Cc7-k7oktS-brYRtD-bCR8h2-4dbvh7-8hVt7E-aE62qi-eifAwL-aBEbVY-92YZHE-arRMAa-7iTLCe-ffFGB2-4tvS5X-aNu3a4-8PqiK7-6ihcde" target="_blank" title="Jim Campbell’s Scattered Light at Northern Spark (2011) photographed by Tony Webster">Background image</a> by Tony Webster</div>'
+                content: 'I’m a UI Developer and Front End Software Engineer with more than a decade of experience crafting rapid prototypes, high volume pixel-perfect advertising and marketing sites, and elegant, user-friendly responsive interfaces for online marketplaces. My skill set includes expert level CSS and HTML, JavaScript, jQuery, and PHP (Zend 1.12 and Laravel). Additional tools include Adobe CC and a strong sense of typography and design, MySQL, Git, Sass, Foundation 5, Bootstrap, Bower and Grunt. I’m in the process of learning Node.js and React, too. When I am not busy coding, some of the things I like to do best are playing guitar and my stand up bass, home recording, or watching my son play baseball. I also love going out to see live music and comedy.<br><br>For more details about my experience, please see <a href="/assets/rickwjones_res2_2016_NC.pdf" target="_blank" title="PDF resume for Rick W. Jones">my resume</a> or visit <a href="http://www.linkedin.com/pub/rick-jones/2/893/563/en" target="_blank">my LinkedIn page</a>.<br><br><div class="credit"><a href="https://www.flickr.com/photos/diversey/5800134409/in/photolist-9QxcTp-77RgMd-4DzDuA-6cq64C-6ckToX-gG9NrM-45GWU1-8yxDLp-94HXqq-eL7G5C-9EQp1w-9irQkD-a8ek2A-4Ub7AR-a8ejF9-a88npx-f6C4sR-nsdCV3-5J3Sr4-7qDFP8-6bMhqd-8T69tw-fnjckt-93j1vo-eWRK6c-gG9Q2a-5vUnQD-93FkxB-79YrQ5-3UHHsL-616R88-eJepFP-857WUC-9a9Cc7-k7oktS-brYRtD-bCR8h2-4dbvh7-8hVt7E-aE62qi-eifAwL-aBEbVY-92YZHE-arRMAa-7iTLCe-ffFGB2-4tvS5X-aNu3a4-8PqiK7-6ihcde" target="_blank" title="Jim Campbell’s Scattered Light at Northern Spark (2011) photographed by Tony Webster">Background image</a> by Tony Webster</div>'
             },
             'contact': {
                 label: 'Rick W. Jones',
@@ -170,25 +170,25 @@ website.config(['$routeProvider',
             });
     }]);*/
 
-function goToSubpage(page) {
-
-    var ahc = {
-            label: 'American Heritage Chocolate',
-            sublabel: 'Portfolio',
-            content: '<div class="row-fluid"><div class="span4"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/05/AHC-timeline-screencap1.jpg" alt="Parallax scrolling timeline" class="single-thumbnail featured"></div><div class="span6 post-aside"><h3>MARS: American Heritage Chocolate</h3><div class="about-project"><p>Front end HTML and CSS for <a href="http://americanheritagechocolate.com" target="_blank" title="Go to MARS American Heritage Chocolate site">American Heritage Chocolate</a> using Foundation 4 CSS framework for a responsive web experience.</p><p>I also helped create the parallax scrolling <a href="http://americanheritagechocolate.com/home/history" title="AHC: History of Chocolate" target="_blank">History of Chocolate</a>, an adaptive interactive timeline using HTML5, CSS3 and jQuery Animation.</p></div></div></div><div class="row-fluid"><div class="span12 entry-content"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/09/American-Heritage-Chocolate.jpg" alt="Homepage American Heritage Chocolate"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/05/mars2.jpg" alt="mars2"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/05/mars-ahc.digital.promocampaigns.comPHASE2.png" alt="Phase 2 lo-fi wireframes using Foundation4."><div class="loop-nav"><div class="previous"><a id="alz" onclick="goToSubpage(\'alz\')" rel="prev">Alzheimer’s Association: The Longest Day →</a></div></div></div></div>'
-        },
-        alz = {
-            label: 'American Heritage Chocolate',
-            sublabel: 'Portfolio',
-            content: '<div class="row-fluid"><div class="span4"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/05/alz.jpg" alt="Alzheimer’s Association: The Longest Day" class="single-thumbnail featured"></div><div class="span6 post-aside"><h3>Alzheimer’s Association: The Longest Day</h3><div class="about-project"><p>I coded the HTML and CSS on this Responsive site for the <a href="http://act.alz.org/site/TR?fr_id=6650&pg=entry" target="_blank" title="Go to Alzheimer’s Association: The Longest Day site">Alzheimer’s Association “The Longest Day” campaign</a>. <p>Our site helped The Alzheimer’s Association exceed this year’s fundraising goal of $1M by over 30% ($1,302,817 total).</p></div></div></div><div class="row-fluid"><div class="span12 entry-content"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/06/alz-tablet.jpg" alt="Tablet view"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/06/alz-phone.jpg" alt="iPhone"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/05/Alzheimers-Association-The-Longest-Day8.png" alt="Thank you"><div class="loop-nav"><div class="previous"><a href="javascript:void(0)" onclick="goToSubpage(\'mss\')" rel="prev">Be The Match: My Social Stand →</a></div></div></div></div>'
-        }
-
-    $('#page').hide();
-    if (page === 'ahc') {
-        $('#subpage').show('slow').html(ahc.content);
-    } else if (page === 'alz') {
-        $('#subpage').hide().show('slow').html(alz.content);
-    }
+//function goToSubpage(page) {
+//
+//    var ahc = {
+//            label: 'American Heritage Chocolate',
+//            sublabel: 'Portfolio',
+//            content: '<div class="row-fluid"><div class="span4"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/05/AHC-timeline-screencap1.jpg" alt="Parallax scrolling timeline" class="single-thumbnail featured"></div><div class="span6 post-aside"><h3>MARS: American Heritage Chocolate</h3><div class="about-project"><p>Front end HTML and CSS for <a href="http://americanheritagechocolate.com" target="_blank" title="Go to MARS American Heritage Chocolate site">American Heritage Chocolate</a> using Foundation 4 CSS framework for a responsive web experience.</p><p>I also helped create the parallax scrolling <a href="http://americanheritagechocolate.com/home/history" title="AHC: History of Chocolate" target="_blank">History of Chocolate</a>, an adaptive interactive timeline using HTML5, CSS3 and jQuery Animation.</p></div></div></div><div class="row-fluid"><div class="span12 entry-content"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/09/American-Heritage-Chocolate.jpg" alt="Homepage American Heritage Chocolate"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/05/mars2.jpg" alt="mars2"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/05/mars-ahc.digital.promocampaigns.comPHASE2.png" alt="Phase 2 lo-fi wireframes using Foundation4."><div class="loop-nav"><div class="previous"><a id="alz" onclick="goToSubpage(\'alz\')" rel="prev">Alzheimer’s Association: The Longest Day →</a></div></div></div></div>'
+//        },
+//        alz = {
+//            label: 'American Heritage Chocolate',
+//            sublabel: 'Portfolio',
+//            content: '<div class="row-fluid"><div class="span4"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/05/alz.jpg" alt="Alzheimer’s Association: The Longest Day" class="single-thumbnail featured"></div><div class="span6 post-aside"><h3>Alzheimer’s Association: The Longest Day</h3><div class="about-project"><p>I coded the HTML and CSS on this Responsive site for the <a href="http://act.alz.org/site/TR?fr_id=6650&pg=entry" target="_blank" title="Go to Alzheimer’s Association: The Longest Day site">Alzheimer’s Association “The Longest Day” campaign</a>. <p>Our site helped The Alzheimer’s Association exceed this year’s fundraising goal of $1M by over 30% ($1,302,817 total).</p></div></div></div><div class="row-fluid"><div class="span12 entry-content"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/06/alz-tablet.jpg" alt="Tablet view"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/06/alz-phone.jpg" alt="iPhone"><img src="http://rickwjones.com/wp/wp-content/uploads/2013/05/Alzheimers-Association-The-Longest-Day8.png" alt="Thank you"><div class="loop-nav"><div class="previous"><a href="javascript:void(0)" onclick="goToSubpage(\'mss\')" rel="prev">Be The Match: My Social Stand →</a></div></div></div></div>'
+//        }
+//
+//    $('#page').hide();
+//    if (page === 'ahc') {
+//        $('#subpage').show('slow').html(ahc.content);
+//    } else if (page === 'alz') {
+//        $('#subpage').hide().show('slow').html(alz.content);
+//    }
 
 
     /*$('#' + page).on('click', function() {
@@ -201,20 +201,13 @@ function goToSubpage(page) {
 
     });*/
 
-};
+//};
 
-$('.nav a').on('click', function() {
-    $('#page').show('slow');
-    $('#subpage').hide();
-});
+//$('.nav a').on('click', function() {
+//    $('#page').show('slow');
+//    $('#subpage').hide();
+//});
 
-// keyboard accessibility for nav
-$('.nav a').on('keypress', function( event ) {
-    if ( event.which == 13 ) {
-        $('#page').show('slow');
-        $('#subpage').hide();
-    }
-});
 
 $(function() {
     if ( document.location.href.indexOf('#Contact') > -1 ) {
